ad5615192afbc2954dbcf7e9ba6b0d0eb5c830b7
SosiskaKillers <Artemijepolikarpov@yandex.ru>
2025-05-15 11:18:08 +0300

    deploy file

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 import sys
+from app import main_app as application
 
 # Add your project directory to the Python path
 path = 'C:/Users/pesas/OneDrive/Desktop/Polikarpov-web-dev-2025-2'
 if path not in sys.path:
     sys.path.append(path)
-
-from app import app as application 
