9dad6dee7dbaaaa749b68f773deec2d62dcba4a2
SosiskaKillers <Artemijepolikarpov@yandex.ru>
2025-05-18 18:29:48 +0300

    return

--- lab2/app.py
+++ lab2/app.py
@@ -1,25 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import re
 
-app = Flask(__name__, template_folder='templates')
-app.config['APPLICATION_ROOT'] = '/lab2'
+app = Flask(__name__, template_folder='templates') # шаблоны хранятся в templates
 
 def validate_phone_number(phone):
+    # Удаляем все разрешенные символы, кроме цифр
     digits = re.sub(r'[+\s\(\)\-\.]', '', phone)
+    
+    # Проверяем наличие недопустимых символов
     if not re.match(r'^[\d\s\(\)\-\.\+]+$', phone):
         return False, "Недопустимый ввод. В номере телефона встречаются недопустимые символы."
+    
+    # Проверяем количество цифр
     if phone.startswith(('+7', '8')):
         if len(digits) != 11:
             return False, "Недопустимый ввод. Неверное количество цифр."
     else:
         if len(digits) != 10:
             return False, "Недопустимый ввод. Неверное количество цифр."
+    
     return True, None
 
 def format_phone_number(phone):
+    # Удаляем все нецифровые символы
     digits = re.sub(r'\D', '', phone)
+    
+    # Форматируем номер в формат 8-***-***-**-**
     if len(digits) == 11:
-        digits = digits[1:]
+        digits = digits[1:]  # Убираем первую цифру, если номер начинается с 8 или +7
     return f"8-{digits[:3]}-{digits[3:6]}-{digits[6:8]}-{digits[8:]}"
 
 @app.route('/')
@@ -34,10 +42,12 @@ def phone():
 def validate_phone():
     phone_number = request.form.get('phone', '')
     is_valid, error_message = validate_phone_number(phone_number)
+    
     if not is_valid:
         return render_template('phone.html', 
                              error_message=error_message,
                              phone=phone_number)
+    
     formatted_phone = format_phone_number(phone_number)
     return render_template('phone.html', 
                          formatted_phone=formatted_phone,
@@ -51,8 +61,13 @@ def cookies():
 def set_cookie():
     cookie_name = request.form.get('cookie_name')
     cookie_value = request.form.get('cookie_value')
+    
     if not cookie_name or not cookie_value:
         return render_template('cookies.html')
+    
+    if cookie_name in request.cookies:
+        return render_template('cookies.html')
+    
     response = make_response(render_template('cookies.html'))
     response.set_cookie(cookie_name, cookie_value)
     return response
@@ -60,6 +75,13 @@ def set_cookie():
 @app.route('/delete_cookie', methods=['POST'])
 def delete_cookie():
     cookie_name = request.form.get('cookie_name')
+    
+    if not cookie_name:
+        return render_template('cookies.html')
+    
+    if cookie_name not in request.cookies:
+        return render_template('cookies.html')
+    
     response = make_response(render_template('cookies.html'))
     response.delete_cookie(cookie_name)
     return response
@@ -74,7 +96,6 @@ def url_params():
 def headers():
     return render_template('headers.html',
                          headers=dict(request.headers))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
